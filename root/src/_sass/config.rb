9cee44bb8ef82976ce3f58962f0d05b9f82227ce
y.saito <y.saito16@gmail.com>
2014-04-10 23:37:53 +0900

    First Commit

---
+++ root/src/_sass/config.rb
@@ -0,0 +1,13 @@
+http_path = "../"
+relative_assets = true
+css_dir = "bin/assets/stylesheets"
+sass_dir = "src/_sass"
+cache_dir = "src/_sass-cache"
+images_dir = "bin/assets/images"
+generated_images_dir = "bin/assets/images/"
+javascripts_dir = "bin/assets/javascripts"
+output_style = :expanded
+#output_style = :compressed
+line_comments = false
+#sass_options = { :debug_info => true }
+asset_cache_buster :none
